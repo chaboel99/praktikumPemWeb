108ad746389c4ddb99ff54e97d8b47db068d8450
chandra <71588049+chaboel99@users.noreply.github.com>
2023-07-01 14:30:18 +0700

    Delete config.php

--- uas/admin/config.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-    $koneksi = mysqli_connect
-    ("localhost","root","","barang_db");
-
-    if (mysqli_connect_error()){
-        echo "Koneksi database gagal : " . mysqli_connect_error();
-    }
-?>
